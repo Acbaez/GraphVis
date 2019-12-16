c1977c54d385d9d4340e6827990d162590ed1a95
Andres Baez <acbaez@ucsd.edu>
2019-12-16 11:32:34 -0800

    debugging

--- Graph/Graph.js
+++ Graph/Graph.js
@@ -1,19 +1,18 @@
 // For file I/O
-//const fs = require('fs');
-//const readline = require('readline');
-const lineReader = require('line-reader');
-
+var lineReader = require('line-reader');
+var Dict = require("dict");
+const fs = require('fs')
 // Define Vertex Class
 class vertex {
   // Constructor Function
   constructor(name) {
-    this.prev = 0;
+    this.prev = 0;  
     this.dist = 0;
     this.x = 0; 
     this.y = 0; 
     this.done = false;
     this.name = name;
-    this.neighbors = {};
+    this.neighbors = new Dict({});
   }
 }
 // Define Graph Class
@@ -21,19 +20,20 @@ class Graph {
   // Graph Constructor
   constructor() {
     // Hashmap for graph vertices
-    this.places = {};
+    this.places = new Dict({});
   }
   // Insert function
-  insert(vertex, x, y) {
-    if (this.vertices[vertex] == null) {
+  insert(vertexName, x, y) {
+    if (this.places.has(vertexName) == false) {
             // change x and y coordinates to integer
-            x_coordinate = parseInt(x, 10); 
-            y_coordinate = parseInt(y, 10); 
+            var x_coordinate = parseInt(x, 10); 
+            var y_coordinate = parseInt(y, 10); 
             // create new vertex
-            newVertex = new vertex(vertex); 
+            var newVertex = new vertex(vertexName); 
             newVertex.x = x_coordinate; 
             newVertex.y = y_coordinate; 
-            this.vertices[vertex] = newVertex; 
+            this.places[vertexName] = newVertex; 
+            //this.places.add(newVertex, vertexName); 
     }
     else { 
         // vertex already exists in graph
@@ -44,12 +44,12 @@ class Graph {
   // Function for creating edges between nodes
   insertEdge(vertex1, vertex2){
     // check if both vertices are in the graph
-    startNode = this.vertices[vertex1]; 
-    endNode = this.vertices[vertex2]; 
-    if(startNode == null || endNode == null){
-        console.log("Cannot find both vertices!"); 
+    if(this.places.has(vertex1) == false || this.places.has(vertex2) == false){
+        //console.log("Cannot find both vertices!"); 
         return; 
     }
+    startNode = this.vertices[vertex1]; 
+    endNode = this.vertices[vertex2]; 
     // calculate euclidean distance and add weight to edge
     var weight = (math.pow((startNode.x - endNode.x),2) + 
         math.pow((startNode.y - endNode.y),2)); 
@@ -57,35 +57,44 @@ class Graph {
     startNode.neighbors[endNode.name] = weight;
     endNode.neighbors[startNode.name] = weight; 
   }
-
-  // Build function 
-  build(inFileVertex, inFilePairs) {
-    // file paths
-    file_path_vertex = inFileVertex; 
-    file_path_pairs = inFilePairs; 
-    // Read each line and add cities into graph
-    lineReader.open(file_path_vertex, function(reader) {
-        // read input file line by line
-        if (reader.hasNextLine()) {
-            reader.nextLine(function(line) {
-                // separate by tab
-                var tabs = item.split('\t');
-                // insert vertex into graph
-                insert(tabs[0], tabs[1], tabs[2]); 
-            });
-        }
-    });
-    // insert edges into graph
-    lineReader.open(file_path_pairs, function(reader) {
-        // read input file line by line
-        if (reader.hasNextLine()) {
-            reader.nextLine(function(line) {
-                // separate by tab
-                var tabs = item.split('\t');
-                // insert vertex into graph
-                insertEdge(tabs[0], tabs[1]); 
-            });
-        }
-    });
-  }
 }
+// Build function 
+function build(inFileVertex, inFilePairs, graph) {
+    // file paths
+    var file_path_vertex = inFileVertex; 
+    var file_path_pairs = inFilePairs; 
+    // Add cities to graph
+    fs.readFile(file_path_vertex, (err, data) => { 
+        if (err){
+            console.log("done");
+            throw err; 
+        } 
+        // separate by tab
+        var data = data.toString(); 
+        var data = data.replace(/\n/g, " "); 
+        var tabs = data.split(' ');
+        for(var i = 0; i < tabs.length-3; i+=3){
+            graph.insert(tabs[i], tabs[i+1], tabs[i+2]); 
+        }
+    })
+    console.log(graph.places)
+    // Add edges to graph
+    fs.readFile(file_path_vertex, (err, data) => { 
+        if (err){
+            console.log("done");
+            throw err; 
+        } 
+        // separate by tab
+        var data = data.toString(); 
+        var data = data.replace(/\n/g, " "); 
+        var tabs = data.split(' ');
+        for(var i = 0; i < tabs.length-2; i+=2){
+            graph.insertEdge(tabs[i], tabs[i+1]); 
+        }
+    })
+}
+
+var testGraph = new Graph(); 
+var file_path1 = "../Data/cityxy.txt"
+var file_path2 = "../Data/citypairs.txt"
+build(file_path1, file_path2, testGraph); 
